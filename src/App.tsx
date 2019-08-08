d8198b8b840fd2c6d49658d831e0bb26549e0e1c
Stuart Tottle <stuarttottle@hotmail.com>
2019-08-08 15:12:08 +0100

    include bitmovin player

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,108 @@
+import { Player, PlayerAPI, PlayerEvent, SourceConfig } from 'bitmovin-player';
 import React from 'react';
 import './App.css';
 
 interface State {
-  isPlaying: boolean;
-  error: any;
+  isPlayingHtml5: boolean;
+  errorHtml5: any;
+  isPlayingBitmovin: boolean;
+  errorBitmovin: any;
+  warningBitmovin: any;
 }
 
 class App extends React.Component<{}, State> {
   private videoEl!: HTMLVideoElement;
+  private bitmovinPlayer!: PlayerAPI;
   state = {
-    error: undefined,
-    isPlaying: false
+    isPlayingHtml5: false,
+    errorHtml5: undefined,
+    isPlayingBitmovin: false,
+    errorBitmovin: undefined,
+    warningBitmovin: undefined
+  };
+  playerConfig = {
+    key: '',
+    ui: false
+  };
+  playerSource: SourceConfig = {
+    dash: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+    hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+    progressive:
+      'https://bitdash-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4',
+    poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg',
+    thumbnailTrack: {
+      url:
+        'https://fs.telecinecloud.com/filmstrips/Telecine_-_Staging/42/317/1759378_54693445892_mp4_video_480x270_272000_all_audio_1.vtt'
+    }
   };
 
   onRefVideo = (el: HTMLVideoElement) => {
     this.videoEl = el;
-    this.playVideo();
+    this.playHtml5Video();
   };
 
-  private playVideo = () => {
+  onRefBitmovinPlayer = (el: HTMLDivElement) => {
+    this.bitmovinPlayer = new Player(el, this.playerConfig);
+    this.bitmovinPlayer.on(PlayerEvent.Warning, error =>
+      this.setState({
+        warningBitmovin: error
+      })
+    );
+
+    this.bitmovinPlayer.load(this.playerSource).then(
+      () => {
+        console.log('Successfully loaded source');
+        this.playBitmovinVideo();
+      },
+      () => {
+        console.log('Error while loading source');
+      }
+    );
+  };
+
+  private play = () => {
+    this.playHtml5Video();
+    this.playBitmovinVideo();
+  };
+
+  private playHtml5Video = () =>
     this.videoEl
       .play()
       .then(() =>
         this.setState({
-          isPlaying: true,
-          error: undefined
+          isPlayingHtml5: true,
+          errorHtml5: undefined
         })
       )
       .catch((error: DOMException) =>
         this.setState({
-          isPlaying: false,
-          error: {
+          isPlayingHtml5: false,
+          errorHtml5: {
             message: error.message,
             name: error.name
           }
         })
       );
-  };
+
+  private playBitmovinVideo = () =>
+    this.bitmovinPlayer
+      .play()
+      .then(() =>
+        this.setState({
+          isPlayingBitmovin: true,
+          errorBitmovin: undefined,
+          warningBitmovin: undefined
+        })
+      )
+      .catch(error =>
+        this.setState({
+          isPlayingBitmovin: false,
+          errorBitmovin: {
+            message: error.message,
+            name: error.name
+          }
+        })
+      );
 
   render() {
     return (
@@ -65,7 +131,11 @@ class App extends React.Component<{}, State> {
 
         <hr />
 
-        <button onClick={this.playVideo}>Click me to Play the Video</button>
+        <div id="bitMovinPlayer" ref={this.onRefBitmovinPlayer} />
+
+        <hr />
+
+        <button onClick={this.play}>Click me to Play the Video</button>
         <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
