9bce4c433622cdaa6801632a8c20f0b5cc7caba0
Stuart Tottle <stuart.tottle@ldn-m-0013.home>
2019-08-08 11:39:14 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+interface State {
+  isPlaying: boolean;
+  error: any;
+}
+
+class App extends React.Component<{}, State> {
+  private videoEl!: HTMLVideoElement;
+  state = {
+    error: undefined,
+    isPlaying: false
+  };
+
+  onRefVideo = (el: HTMLVideoElement) => {
+    this.videoEl = el;
+    this.playVideo();
+  };
+
+  private playVideo = () => {
+    this.videoEl
+      .play()
+      .then(() =>
+        this.setState({
+          isPlaying: true,
+          error: undefined
+        })
+      )
+      .catch((error: DOMException) =>
+        this.setState({
+          isPlaying: false,
+          error: {
+            message: error.message,
+            name: error.name
+          }
+        })
+      );
+  };
+
+  render() {
+    return (
+      <div className="App">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Go to <code>chrome://flags/</code> and set the <code>Autoplay policy</code> to{' '}
+          <code>Document user activation is required</code> to view the error (refresh the page)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <p>
+          Info:
+          <a href="https://developers.google.com/web/updates/2017/09/autoplay-policy-changes">
+            https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+          </a>
+        </p>
+
+        <hr />
+
+        <video
+          ref={this.onRefVideo}
+          width="620"
+          poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <source src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4" type="video/mp4" />
+          Your browser doesn't support HTML5 video tag.
+        </video>
+
+        <hr />
+
+        <button onClick={this.playVideo}>Click me to Play the Video</button>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
+      </div>
+    );
+  }
 }
 
 export default App;
